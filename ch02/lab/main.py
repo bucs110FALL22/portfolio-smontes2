6eb72ce34a9f282ccae6372531e66922c8273811
Samuel Montes <112510344+smontes2@users.noreply.github.com>
2022-09-07 20:04:08 +0000

    Chapter 2 Lab complete

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,3 +1,4 @@
+import random
 
 #Part A
 weeks = 16
@@ -6,5 +7,26 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = 14
+
+cost_per_class = (cost_per_week / classes_per_week)
+
+print("Here is your cost per class:" , cost_per_class , ", have a nice day! ")
+
+print(weeks , type(weeks))
+print(classes , type(classes))
+print(tuition , type(tuition))
+print(cost_per_week , type(cost_per_week))
+print(classes_per_week , type(classes_per_week))
+print(cost_per_class , type(cost_per_class))
+
 
 #Part B
+
+mySports = ["Baseball" , "Track" ,  "Swimming" , "Football" , "Hockey"]
+
+mySportsRandom = random.choice(mySports)
+
+print(mySportsRandom)
+
+
