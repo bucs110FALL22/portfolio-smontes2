af646659cd74f1853dea396f07c551b13344b97e
Samuel Montes <112510344+smontes2@users.noreply.github.com>
2022-10-15 03:39:38 +0000

    Chapter 5 lab - incomplete

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,12 +1,13 @@
 import random
 
 n = random.randrange(1, 101, 1)
+print(n)
 
 while n > 1:
- for i in range(n):
+ for i in range(int(n)):
    if n % 2 == 0:
      n = n / 2
      print(n)
    elif n % 3 == 1:
-     n = n * 3 + 1
+     n = ((n * 3) + 1)
      print(n)
