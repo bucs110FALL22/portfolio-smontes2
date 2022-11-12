6da85d686b0f2c9f8ef949ac6fb9b6320c33e563
Samuel Montes <112510344+smontes2@users.noreply.github.com>
2022-11-12 00:47:55 +0000

    Chapter 7 lab redo

--- ch07/lab/main.py
+++ ch07/lab/main.py
@@ -4,18 +4,19 @@ from surface import surface
 r = rectangle(10, 10, 10, 10)
 assert((r.x, r.y, r.height, r.width) == (10,10,10,10))
 r = rectangle(-1, 1, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (-1,1,1,1))
+assert((r.x, r.y, r.height, r.width) == (0,1,1,1))
 r = rectangle(1, -1, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,-1,1,1))
+assert((r.x, r.y, r.height, r.width) == (1,0,1,1))
 r = rectangle(1, 1, -1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,1,-1,1))
+assert((r.x, r.y, r.height, r.width) == (1,1,0,1))
 r = rectangle(1, 1, 1, -1)
-assert((r.x, r.y, r.height, r.width) == (1,1,1,-1))
+assert((r.x, r.y, r.height, r.width) == (1,1,1,0))
 
 s = surface("myimage.png", 10, 10, 10, 10)
 assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10))
-s.rect = s.getRect()
-assert(s.rect.x, s.getRect().y, s.rect.height, s.rect.width) == (10,10,10,10)
+srect = s.getRect()
+assert(srect.x, s.getRect().y, srect.height,  srect.width) == (10,10,10,10) 
 assert s.image 
 print("Test Complete!")
+
 
--- ch07/lab/rectangle.py
+++ ch07/lab/rectangle.py
@@ -1,10 +1,13 @@
 class rectangle:
 
     def __init__(self, x, y, h, w):
-        self.x = x
-        self.y = y
-        self.height = h
-        self.width = w
-
+        self.x = max(0,x)
+        self.y = max(0,y)
+        self.height = max(0,h)
+        self.width = max(0,w) 
+        
     def __str__(self):
-        return self.x, self.y, self.height, self.width
+        return f"(x:{self.x},y:{self.y})height: {self.height}, width:{self.width}"
+
+"def __str__ returns a string containing the x, y, width, and height of the rectangle."
+"returns the string coordinates"
